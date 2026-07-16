e98e0f2ea1c9b2061c9a9251531d12fcd88c07ef
mzyc2 <13269995851@163.com>
2026-07-16 11:50:47 +0800

    解决引用依赖报错的问题

--- plane.js/src/index.js
+++ plane.js/src/index.js
@@ -1,42 +1,97 @@
-import './core/Plane.js';
+window.plane = {
+    version: "1.0.0",
+    author: "plane.js Team",
+    license: "MIT",
+    _initialized: false,
+    _main_loop: null,
+    _root_node: null,
+    get initialized() { return this._initialized; },
+    get main_loop() { return this._main_loop; },
+    get root_node() { return this._root_node; }
+};
 
-import './math/Vector2.js';
-import './math/Rect2.js';
-import './math/Transform2D.js';
-import './math/Color.js';
-import './math/MathUtils.js';
+window.global_events = {
+    _signals: {},
+    get_signal(name) {
+        if (!this._signals[name]) this._signals[name] = {
+            _listeners: [],
+            _once_listeners: [],
+            connect(callback, target = null) { this._listeners.push({ callback, target }); },
+            connect_once(callback, target = null) { this._once_listeners.push({ callback, target }); },
+            disconnect(callback, target = null) {
+                this._listeners = this._listeners.filter(l => l.callback !== callback || (target !== null && l.target !== target));
+                this._once_listeners = this._once_listeners.filter(l => l.callback !== callback || (target !== null && l.target !== target));
+            },
+            emit(...args) {
+                for (const listener of this._listeners) listener.callback.apply(listener.target, args);
+                for (const listener of this._once_listeners) listener.callback.apply(listener.target, args);
+                this._once_listeners = [];
+            }
+        };
+        return this._signals[name];
+    },
+    connect(signal_name, callback, target = null) { this.get_signal(signal_name).connect(callback, target); },
+    connect_once(signal_name, callback, target = null) { this.get_signal(signal_name).connect_once(callback, target); },
+    disconnect(signal_name, callback, target = null) { if (this._signals[signal_name]) this._signals[signal_name].disconnect(callback, target); },
+    emit(signal_name, ...args) { if (this._signals[signal_name]) this._signals[signal_name].emit(...args); },
+    clear_signal(signal_name) { if (this._signals[signal_name]) { this._signals[signal_name]._listeners = []; this._signals[signal_name]._once_listeners = []; } },
+    clear_all() { this._signals = {}; }
+};
 
-import './core/EventBus.js';
-import './core/node.js';
-import './core/MainLoop.js';
-import './core/SceneTree.js';
+const basePath = import.meta.url.substring(0, import.meta.url.lastIndexOf('/') + 1);
 
-import './scene/Scene.js';
+function loadScriptSync(src) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', basePath + src, false);
+    xhr.send();
+    let code = xhr.responseText;
+    code = code.replace(/export\s+default\s+\w+;\s*/g, '');
+    code = code.replace(/export\s*\{\s*[\w\s,]*\s*\};\s*/g, '');
+    const script = document.createElement('script');
+    script.textContent = code;
+    document.head.appendChild(script);
+}
 
-import './camera/Camera2D.js';
+loadScriptSync('./math/Vector2.js');
+loadScriptSync('./math/Rect2.js');
+loadScriptSync('./math/Transform2D.js');
+loadScriptSync('./math/Color.js');
+loadScriptSync('./math/MathUtils.js');
+loadScriptSync('./core/EventBus.js');
+loadScriptSync('./core/node.js');
+loadScriptSync('./core/SceneTree.js');
+loadScriptSync('./core/MainLoop.js');
+loadScriptSync('./nodes/Node2D.js');
+loadScriptSync('./core/engine.js');
+loadScriptSync('./input/Input.js');
+loadScriptSync('./audio/AudioStreamPlayer.js');
+loadScriptSync('./scene/Scene.js');
+loadScriptSync('./camera/Camera2D.js');
+loadScriptSync('./renderer/Renderer.js');
+loadScriptSync('./nodes/Sprite.js');
+loadScriptSync('./nodes/AnimatedSprite.js');
+loadScriptSync('./nodes/Label.js');
+loadScriptSync('./nodes/ColorRect.js');
+loadScriptSync('./nodes/TextureRect.js');
+loadScriptSync('./nodes/Container.js');
+loadScriptSync('./nodes/TileMap.js');
+loadScriptSync('./nodes/Particle2D.js');
+loadScriptSync('./components/SpriteRenderer.js');
+loadScriptSync('./physics/RigidBody2D.js');
+loadScriptSync('./animation/Animation.js');
+loadScriptSync('./resources/ResourceLoader.js');
+loadScriptSync('./ui/Control.js');
 
-import './renderer/Renderer.js';
+const __saved_classes = {};
+const __skip_keys = ['version', 'author', 'license', '_initialized', '_main_loop', '_root_node', 'initialized', 'main_loop', 'root_node'];
+for (const key of Object.keys(window.plane)) {
+    if (!__skip_keys.includes(key)) {
+        __saved_classes[key] = window.plane[key];
+    }
+}
 
-import './nodes/Node2D.js';
-import './nodes/Sprite.js';
-import './nodes/AnimatedSprite.js';
-import './nodes/Label.js';
-import './nodes/ColorRect.js';
-import './nodes/TextureRect.js';
-import './nodes/Container.js';
-import './nodes/TileMap.js';
-import './nodes/Particle2D.js';
+loadScriptSync('./core/Plane.js');
 
-import './components/SpriteRenderer.js';
-
-import './physics/RigidBody2D.js';
-
-import './input/Input.js';
-
-import './animation/Animation.js';
-
-import './audio/AudioStreamPlayer.js';
-
-import './resources/ResourceLoader.js';
-
-import './ui/Control.js';
+for (const key of Object.keys(__saved_classes)) {
+    window.plane[key] = __saved_classes[key];
+}
